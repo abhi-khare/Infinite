f6edc10b9db77e37eb99b0ba412ea9452a583703
Khare <abhkhare@a483e73d625d.ant.amazon.com>
2020-12-19 10:46:41 +0530

    bash file to run various experiment

---
+++ config_run.sh
@@ -0,0 +1,1 @@
+python IC_NER_Training.py --train_dir ./data/splits/multi-train.tsv --val_dir ./data/splits/multi-dev.tsv --exp_name exp1 --slot_dropout 0.2442 --intent_dropout 0.3803 --encoder_lr 0.000108 --lr_rest 0.000329
