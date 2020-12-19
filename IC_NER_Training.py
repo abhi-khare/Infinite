e8529aab6e48f70f8a71dff3c337508a1ec75142
Khare <abhkhare@a483e73d625d.ant.amazon.com>
2020-12-19 17:16:48 +0530

    mixed precision ermoved

--- IC_NER_Training.py
+++ IC_NER_Training.py
@@ -74,11 +74,11 @@ if args.freeze_encoder:
         params.requires_grad = False
 
 # optimizer
-optimizer =  optim.Adam([{'params': model.encoder.parameters(), 'lr': args.lr_encoder}], lr=args.lr_rest,weight_decay=1e-3)
+optimizer =  optim.Adam([{'params': model.encoder.parameters(), 'lr': args.encoder_lr}], lr=args.rest_lr,weight_decay=1e-3)
 
 # training loop
 print('*'*10  + 'Training loop started' + '*'*10)
-scaler = torch.cuda.amp.GradScaler()
+#scaler = torch.cuda.amp.GradScaler()
 for _ in range(1,args.epoch):
 
     epoch_loss,num_batch = 0.0,0
@@ -94,14 +94,14 @@ for _ in range(1,args.epoch):
         slots_label = batch['slots_label']
         slots_mask = batch['slots_mask'].to(args.device, dtype = torch.long)
         # zero the parameter gradients
-        with torch.cuda.amp.autocast():
-            joint_loss ,sp,ip = model(token_ids,mask,intent_target,slots_target,slots_mask)
+        #with torch.cuda.amp.autocast():
+        joint_loss ,sp,ip = model(token_ids,mask,intent_target,slots_target,slots_mask)
         
-        scaler.scale(joint_loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
-        #joint_loss.backward()
-        #optimizer.step()
+        #scaler.scale(joint_loss).backward()
+        #scaler.step(optimizer)
+        #scaler.update()
+        joint_loss.backward()
+        optimizer.step()
         optimizer.zero_grad()
 
         epoch_loss += joint_loss.detach()
