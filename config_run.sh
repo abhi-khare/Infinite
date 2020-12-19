bd7d035a49720c411a242f0927fb7ed507d28cca
Khare <abhkhare@a483e73d625d.ant.amazon.com>
2020-12-19 18:48:35 +0530

    exp2 config added

--- config_run.sh
+++ config_run.sh
@@ -1,3 +1,5 @@
 #python IC_NER_Training.py --train_dir ./data/splits/multi-train.tsv --val_dir ./data/splits/multi-dev.tsv --exp_name exp1 --slots_dropout 0.2442 --intent_dropout 0.3803 --encoder_lr 0.000108 --rest_lr 0.000329
 
-python mlm_pretraining.py --train_dir ./data/splits/text/multi-train_text.txt --val_dir ./data/splits/text/multi-dev_text.txt --export_dir ./bin
+#python mlm_pretraining.py --train_dir ./data/splits/text/multi-train_text.txt --val_dir ./data/splits/text/multi-dev_text.txt --export_dir ./bin
+
+python IC_NER_Training.py --train_dir ./data/splits/multi-train.tsv --val_dir ./data/splits/multi-dev.tsv --exp_name exp1 --slots_dropout 0.2442 --intent_dropout 0.3803 --encoder_lr 0.000108 --rest_lr 0.000329 --model_weights ./bin/checkpoint-5000
