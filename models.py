026be65a987b2ca85e6d6db8537781f9aac9a848
Khare <abhkhare@a483e73d625d.ant.amazon.com>
2020-12-22 13:50:58 +0530

    extra linear layer added for slots prediction

--- models.py
+++ models.py
@@ -21,9 +21,11 @@ class jointBert(nn.Module):
         
         
         # slots layer
-        self.slots_classifier = nn.Linear(768, args.slots_num)
-        #p_slots = trial.suggest_float("slot_dropout", 0.1, 0.4)
         self.slots_dropout = nn.Dropout(args.slots_dropout_val)
+        self.slots_classifier_1 = nn.Linear(768, 256)
+        self.slots_classifier_2 = nn.Linear(256, args.slots_num)
+        #p_slots = trial.suggest_float("slot_dropout", 0.1, 0.4)
+        
 
         self.crf = CRF(args.slots_num)
 
@@ -38,7 +40,7 @@ class jointBert(nn.Module):
 
         #intent data flow
         intent_hidden = encoded_output[0][:,0]
-        intent_logits = self.intent_linear_1(self.intent_dropout(F.relu(intent_hidden)))
+        intent_hidden = self.intent_linear_1(self.intent_dropout(F.relu(intent_hidden)))
         intent_logits = self.intent_linear_2(F.relu(intent_hidden))
         # accumulating intent classification loss 
         intent_loss = self.intent_loss(intent_logits, intent_target)
@@ -46,7 +48,9 @@ class jointBert(nn.Module):
         
         # slots data flow 
         slots_hidden = encoded_output[0]
-        slots_logits = self.slots_classifier(self.slots_dropout(slots_hidden))#slots_LN(slots_hidden)))
+        slots_hidden = self.slots_classifier_1(self.slots_dropout(F.relu(slots_hidden)))
+        slots_logits = self.slots_classifier_2(F.relu(slots_hidden))
+
         # accumulating slot prediction loss
         slots_loss = -1 * self.joint_loss_coef * self.crf(slots_logits, slots_target, mask=slots_mask.byte())
         slots_loss = torch.mean(slots_loss)
@@ -65,13 +69,13 @@ class Bertencoder(nn.Module):
         super(Bertencoder,self).__init__()
         
         self.encoder = DistilBertModel.from_pretrained(model,return_dict=True,output_hidden_states=True)
-        self.pre_classifier = torch.nn.Linear(768, 768)
+        self.classifier = torch.nn.Linear(768, 256)
         
     
     def forward(self, input_ids, attention_mask):
 
         encoded_output = self.encoder(input_ids, attention_mask)
-        hidden = self.pre_classifier(encoded_output[0][:,0])
+        hidden = self.classifier(encoded_output[0][:,0])
         
         return hidden
 
