85c6d3581790aec5dd28cb32d6c32784884b3571
Khare <abhkhare@a483e73d625d.ant.amazon.com>
2020-12-22 18:34:21 +0530

    jointBert final run config added

--- config_run.sh
+++ config_run.sh
@@ -1,6 +1,10 @@
-python jointBertTuning.py --train_dir ./data/splits/rich/multi-train.tsv --val_dir ./data/splits/rich/multi-dev.tsv 
+# baseline jointBert model on 4 languages
 
-#python IC_NER_Training.py --train_dir ./data/splits/multi-train.tsv --val_dir ./data/splits/multi-dev.tsv --exp_name exp1 --slots_dropout 0.2442 --intent_dropout 0.3803 --encoder_lr 0.000108 --rest_lr 0.000329
+python IC_NER_Training.py --train_dir ./data/splits/rich/multi-train.tsv --val_dir ./data/splits/rich/multi-dev.tsv --exp_name jointBert --slots_dropout 0.2055 --weight_decay 0.0020 --intent_dropout 0.1529 --intent_lr 0.0000806 --encoder_lr 0.0000315 --slots_lr 0.000309
+
+
+#python jointBertTuning.py --train_dir ./data/splits/rich/multi-train.tsv --val_dir ./data/splits/rich/multi-dev.tsv 
+
 
 #python mlm_pretraining.py --train_dir ./data/splits/text/multi-train_text.txt --val_dir ./data/splits/text/multi-dev_text.txt --export_dir ./bin
 
