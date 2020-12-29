17e2df509e74ea3c8898fc9b49108f5474475f5d
Khare <abhkhare@a483e73d625d.ant.amazon.com>
2020-12-29 17:50:01 +0530

    IC_NER config is updated with tuned HP

--- isolation/config_run.sh
+++ isolation/config_run.sh
@@ -1,6 +1,6 @@
-# baseline jointBert model on 4 languages
+# baseline jointBert model on en dataset
 
-#python IC_NER_Training.py --train_dir ./data/splits/rich/multi-train.tsv --val_dir ./data/splits/rich/multi-dev.tsv --exp_name jointBert --slots_dropout 0.2055 --weight_decay 0.0020 --intent_dropout 0.1529 --intent_lr 0.0000806 --encoder_lr 0.0000315 --slots_lr 0.000309
+python IC_NER_Training.py --train_dir ./data/train_EN.tsv --val_dir ./data/dev_EN.tsv  --exp_name jointBert_EN --slots_dropout 0.1949101 --weight_decay 0.0022488 --intent_dropout 0.25895432 --intent_lr 0.00097826 --encoder_lr 0.000074857785 --slots_lr 0.000744823
 
 # isolated jointBert tuning on en dataset.
-python jointBertTuning.py --train_dir ./data/train_EN.tsv --val_dir ./data/dev_EN.tsv 
+#python jointBertTuning.py --train_dir ./data/train_EN.tsv --val_dir ./data/dev_EN.tsv 
