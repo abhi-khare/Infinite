9fe3481ab1e0cb3a07d0209ab3fae80e01d00a0b
abhinash khare <khare.abhinash4@gmail.com>
2021-04-04 03:44:18 -0700

    argument file updated for jointBert

--- arguments.py
+++ arguments.py
@@ -1,59 +1,22 @@
 import argparse
-
-def ICPT_arguments():
-    parser = argparse.ArgumentParser()
-
-    # model arguments
-    parser.add_argument('--model_name', type=str, default='distilbert-base-multilingual-cased')
-    parser.add_argument('--tokenizer_name', type=str, default='distilbert-base-multilingual-cased')
-    parser.add_argument('--lr',type=float,default=0.00001)
-    parser.add_argument('--weights',type=float,default=0.0001)
-    parser.add_argument('--model_mode', type=str , default='INTENT_CONTRA_MODE')
-
-    # loss function args
-    parser.add_argument('--loss',type=str)
-    parser.add_argument('--margin',type=float,default=2.0)
-    parser.add_argument('--temperature',type=float,default=0.07)
-    parser.add_argument('--neg_margin', type=float , default=2.0)
-    
-
-    # data args
-    parser.add_argument('--max_len',type=int,default=56)
-    parser.add_argument('--train_dir', type=str)
-    parser.add_argument('--val_dir', type=str)
-    parser.add_argument('--intent_num', type=int, default=17)
-    parser.add_argument('--slots_num', type=int , default=160)
-
-    # training args
-    parser.add_argument('--epoch',type=int,default=5)
-
-    # misc. args
-    parser.add_argument('--device', type=str, default='cuda')
-    parser.add_argument('--num_worker', type=int , default=4)
-    parser.add_argument('--model_export', type=str)
-    parser.add_argument('--exp_name',type=str)
-    
-    return parser.parse_args()
-
-
+   
 def jointBert_argument():
     parser = argparse.ArgumentParser()
     
     # model arguments
     parser.add_argument('--model_name', type=str, default='distilbert-base-multilingual-cased')
     parser.add_argument('--tokenizer_name', type=str, default='distilbert-base-multilingual-cased')
-    parser.add_argument('--slots_dropout_val', type=float, default=0.1)
-    parser.add_argument('--intent_dropout_val', type=float, default=0.1)
+    parser.add_argument('--slots_dropout_val', type=float, default=0.25)
+    parser.add_argument('--intent_dropout_val', type=float, default=0.25)
     parser.add_argument('--joint_loss_coef', type=float, default=0.5)
-    parser.add_argument('--freeze_encoder', type=bool , default=False)
     parser.add_argument('--model_mode', type=str , default='IC_NER_MODE')
     
     #training parameters 
     parser.add_argument('--encoder_lr', type=float , default=0.0005)
     parser.add_argument('--intent_lr', type=float , default=0.002)
     parser.add_argument('--slots_lr', type=float , default=0.002)
-    parser.add_argument('--epoch',type=int,default=35)
-    parser.add_argument('--batch_size',type=int,default=128)
+    parser.add_argument('--epoch',type=int,default=19)
+    parser.add_argument('--batch_size',type=int,default=16)
     parser.add_argument('--weight_decay',type=float,default=0.003)
     parser.add_argument('--shuffle_data', type=bool , default=True)
     parser.add_argument('--num_worker', type=int , default=4)
@@ -62,11 +25,16 @@ def jointBert_argument():
     parser.add_argument('--train_dir',type=str)
     parser.add_argument('--val_dir',type=str)
     parser.add_argument('--intent_num', type=int, default=17)
-    parser.add_argument('--slots_num', type=int , default=160)
+    parser.add_argument('--slots_num', type=int , default=159)
     parser.add_argument('--max_len', type=int, default=56)
 
     #misc. 
     parser.add_argument('--device', type=str, default='cuda')
-    parser.add_argument('--exp_name', type=str)
+    parser.add_argument('--log_dir', type=str)
+    parser.add_argument('--weight_dir', type=str)
+    parser.add_argument('--fix_seed', type=bool, default=False)
+    parser.add_argument('--precision', type=int, default=16)
+    parser.add_argument('--accumulate_grad',type=int, default=4)
+    
 
     return parser.parse_args()
