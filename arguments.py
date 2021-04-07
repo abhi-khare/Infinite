01b670ecbe56917395285f7cadbfdd43c873929b
abhinash khare <khare.abhinash4@gmail.com>
2021-04-06 20:49:48 -0700

    argument added

--- arguments.py
+++ arguments.py
@@ -6,15 +6,14 @@ def jointBert_argument():
     # model arguments
     parser.add_argument('--model_name', type=str, default='distilbert-base-multilingual-cased')
     parser.add_argument('--tokenizer_name', type=str, default='distilbert-base-multilingual-cased')
-    parser.add_argument('--slots_dropout_val', type=float, default=0.25)
-    parser.add_argument('--intent_dropout_val', type=float, default=0.25)
-    parser.add_argument('--joint_loss_coef', type=float, default=0.5)
+    parser.add_argument('--joint_loss_coef', type=float, default=0.4)
     parser.add_argument('--model_mode', type=str , default='IC_NER_MODE')
     
     #training parameters 
     parser.add_argument('--encoder_lr', type=float , default=0.0005)
     parser.add_argument('--intent_lr', type=float , default=0.002)
     parser.add_argument('--slots_lr', type=float , default=0.002)
+
     parser.add_argument('--epoch',type=int,default=19)
     parser.add_argument('--batch_size',type=int,default=16)
     parser.add_argument('--weight_decay',type=float,default=0.003)
@@ -24,7 +23,7 @@ def jointBert_argument():
     # data
     parser.add_argument('--train_dir',type=str)
     parser.add_argument('--val_dir',type=str)
-    parser.add_argument('--intent_num', type=int, default=17)
+    parser.add_argument('--intent_num', type=int, default=18)
     parser.add_argument('--slots_num', type=int , default=159)
     parser.add_argument('--max_len', type=int, default=56)
 
@@ -35,6 +34,8 @@ def jointBert_argument():
     parser.add_argument('--fix_seed', type=bool, default=False)
     parser.add_argument('--precision', type=int, default=16)
     parser.add_argument('--accumulate_grad',type=int, default=4)
+
+    parser.add_argument('--test_exp',type=bool, default=True)
     
 
     return parser.parse_args()
