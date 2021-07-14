da7bd0930b2770d39b3303952cf32e0182a75b43
abhinash khare <khare.abhinash4@gmail.com>
2021-07-14 22:17:37 +0530

    mlm training script added

--- mlmTraining.py
+++ mlmTraining.py
@@ -4,22 +4,46 @@ from transformers import DistilBertForMaskedLM
 from transformers import LineByLineTextDataset
 from transformers import DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
+import argparse
+
+def mlm_params():
+
+    parser = argparse.ArgumentParser()
+    
+    # model params
+    parser.add_argument('--encoder', type=str, default='distilbert-base-cased')
+    parser.add_argument('--tokenizer', type=str, default='distilbert-base-cased')
+    
+    # training params
+    parser.add_argument('--epoch', type=int, default=40)
+    parser.add_argument('--batchSize', type=int, default=64)
+    parser.add_argument('--l2', type=float, default=0.003)
+
+    # data params
+    parser.add_argument('--trainDir', type=str)
+    parser.add_argument('--valDir', type=str)
+    
+    # misc. params
+    parser.add_argument('--paramDir', type=str)
+    parser.add_argument('--logDir', type=str)
+    
+    return parser.parse_args()
+
+args = mlm_params()
 
 
-basePath = './'
-
-tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-cased')
-model = DistilBertForMaskedLM.from_pretrained('distilbert-base-cased')
+tokenizer = DistilBertTokenizerFast.from_pretrained(args.tokenizer)
+model = DistilBertForMaskedLM.from_pretrained(args.encoder)
 
 train_dataset = LineByLineTextDataset(
     tokenizer=tokenizer,
-    file_path=basePath + 'data/SNIPS/experiments/train/adversarialAdaptiveBG_train.tsv',
+    file_path=args.trainDir,
     block_size=128,
 )
 
 val_dataset = LineByLineTextDataset(
     tokenizer=tokenizer,
-    file_path= basePath + 'data/SNIPS/experiments/dev/aadversarialAdaptiveDev_BG.tsv',
+    file_path= args.valDir,
     block_size=128,
 )
 
@@ -28,20 +52,20 @@ data_collator = DataCollatorForLanguageModeling(
 )
 
 training_args = TrainingArguments(
-    output_dir= basePath + 'bin/SNIPS/MLM_Adver_Adap_BG/',
-    num_train_epochs=10,
-    per_device_train_batch_size=128,
+    output_dir= args.paramDir,
+    num_train_epochs=args.epochs,
+    per_device_train_batch_size=args.batchSize,
     save_steps=20,
     evaluation_strategy="steps",
     fp16=True,
-    weight_decay=0.003,
+    weight_decay=args.l2,
     load_best_model_at_end=True,
     greater_is_better=False,
     save_total_limit = 1,
-    logging_dir= basePath + 'logs/SNIPS/MLM_Adver_Adap_BG/',
+    logging_dir= args.logDir,
     metric_for_best_model="eval_loss",
     prediction_loss_only=True
-)
+    )
 
 trainer = Trainer(
     model=model,
@@ -49,9 +73,8 @@ trainer = Trainer(
     data_collator=data_collator,
     train_dataset=train_dataset,
     eval_dataset=val_dataset
-    
-)
+    )
 
 trainer.train()
 
-trainer.save_model(basePath + 'bin/SNIPS/MLM_Adver_Adap_BG/best/')
+trainer.save_model(args.paramDir + '/best/')
