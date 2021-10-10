76dce010c87c76be26784be16a78cefedb143fb1
Khare <abhkhare@a483e73d625d.ant.amazon.com>
2021-10-10 09:42:07 +0530

    f1 score metric added

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,12 @@
 import torch 
+from torchmetrics.functional import f1
 
-def accuracy(pred,target):
+def accuracy(pred, target):
+    
     return torch.sum(pred==target)/float(len(target))
+
+def F1(pred, target):
+
+    return f1(pred, target)
+    
 
