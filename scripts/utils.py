1b3a5bbf5b726adfffd42bdabe51aba7a89b4085
Khare <abhkhare@a483e73d625d.ant.amazon.com>
2021-10-21 11:27:20 +0530

    mean , std error calculation function added

--- scripts/utils.py
+++ scripts/utils.py
@@ -8,5 +8,17 @@ def accuracy(pred, target):
 def F1(pred, target):
 
     return f1(pred, target)
+
+def cal_mean_stderror(metric):
+    
+    if len(metric) == 1:
+        return metric
+    var,std_error = 0,0
+    mean = sum(metric)/len(metric)
+    for m in metric:
+        var += (m-mean)**2
+    var = (var/(len(metric)-1))**0.5
+    std_error = var/((len(metric))**0.5)
+    return [round(mean,4),round(std_error,4)]
     
 
