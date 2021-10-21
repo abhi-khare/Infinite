16fc81126885e020be67a6262c58107f47aa7eaa
Khare <abhkhare@a483e73d625d.ant.amazon.com>
2021-10-21 11:24:42 +0530

    collate function for AT added

--- scripts/collatefunc.py
+++ scripts/collatefunc.py
@@ -51,3 +51,41 @@ def collate_sup(batch,tokenizer):
     }
 
     return {"supBatch": supBatch}
+
+def collate_AT(batch,tokenizer,noise_type):
+
+    text,intent_id = [],[]
+    
+    for datapoint in batch:
+        text.append(datapoint['text'])
+        intent_id.append(datapoint['intent_id'])
+    
+    # sampling adversarial examples
+    batch_size = len(text)
+    idx = random.sample(list(range(batch_size)), int(0.30*batch_size))
+
+    text = [sample for id,sample in enumerate(text) if id in idx]
+    intent_id = [sample for id,sample in enumerate(intent_id) if id in idx]
+    
+    # adversarial examples
+    #if noise_type == 'MC':
+    #    noise_param = random.sample([0.20,0.40,0.60],1)[0]
+    #    adv_text,adv_intent,adv_slot = MC_noise(text,intent_id,slot_id,noise_param)
+    #elif noise_type == 'BG':
+    #    noise_param = random.sample([0.25,0.50,0.75],1)[0]
+    #    adv_text,adv_intent,adv_slot = BG_noise(text,intent_id,slot_id,noise_param)
+    
+    text = text + adv_text
+    intent_id = intent_id + adv_intent
+
+    # tokenization    
+    token_ids, mask = batch_tokenizer(text, tokenizer)
+    token_ids, mask, intent_id = list2Tensor([token_ids, mask, intent_id])
+
+    Batch = {
+        "token_ids": token_ids,
+        "mask": mask,
+        "intent_id": intent_id,
+    }
+
+    return {"supBatch": supBatch}
