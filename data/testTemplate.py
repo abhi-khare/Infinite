1faa109bfc8102417b3e831c98b1657ca423dde5
Khare <abhkhare@a483e73d625d.ant.amazon.com>
2021-10-27 11:45:11 +0530

    test template added

--- data/testTemplate.py
+++ data/testTemplate.py
@@ -4,22 +4,76 @@ def test_template(test, basePath):
 
         test_clean = [basePath + "data/ATIS/experiments/clean/test/test.tsv"]
 
+        test_BT_ru_1 = [basePath + "data/ATIS/experiments/back_trans/ru/test_fbwmt19_ru_N3_F1_B1.tsv"]
+        test_BT_ru_4 = [basePath + "data/ATIS/experiments/back_trans/ru/test_fbwmt19_ru_N3_F4_B4.tsv"]
+        test_BT_ru_16 = [basePath + "data/ATIS/experiments/back_trans/ru/test_fbwmt19_ru_N3_F16_B16.tsv"]
+        test_BT_ru_64 = [basePath + "data/ATIS/experiments/back_trans/ru/test_fbwmt19_ru_N3_F64_B64.tsv"]
+
+        test_BT_de_1 = [basePath + "data/ATIS/experiments/back_trans/de/test_fbwmt19_de_N3_F1_B1.tsv"]
+        test_BT_de_4 = [basePath + "data/ATIS/experiments/back_trans/de/test_fbwmt19_de_N3_F4_B4.tsv"]
+        test_BT_de_16 = [basePath + "data/ATIS/experiments/back_trans/de/test_fbwmt19_de_N3_F16_B16.tsv"]
+        test_BT_de_64 = [basePath + "data/ATIS/experiments/back_trans/de/test_fbwmt19_de_N3_F64_B64.tsv"]
+
         return {
             "clean": test_clean,
+            "test_BT_ru_1" : test_BT_ru_1,
+            "test_BT_ru_4" : test_BT_ru_4,
+            "test_BT_ru_16" : test_BT_ru_16,
+            "test_BT_ru_64" : test_BT_ru_64,
+            "test_BT_de_1" : test_BT_de_1,
+            "test_BT_de_4" : test_BT_de_4,
+            "test_BT_de_16" : test_BT_de_16, 
+            "test_BT_de_64" : test_BT_de_64
         }
 
     if test == "SNIPS":
 
         test_clean = [basePath + "data/SNIPS/experiments/clean/test/test.tsv"]
 
+        test_BT_ru_1 = [basePath + "data/SNIPS/experiments/back_trans/ru/test_fbwmt19_ru_N3_F1_B1.tsv"]
+        test_BT_ru_4 = [basePath + "data/SNIPS/experiments/back_trans/ru/test_fbwmt19_ru_N3_F4_B4.tsv"]
+        test_BT_ru_16 = [basePath + "data/SNIPS/experiments/back_trans/ru/test_fbwmt19_ru_N3_F16_B16.tsv"]
+        test_BT_ru_64 = [basePath + "data/SNIPS/experiments/back_trans/ru/test_fbwmt19_ru_N3_F64_B64.tsv"]
+
+        test_BT_de_1 = [basePath + "data/SNIPS/experiments/back_trans/de/test_fbwmt19_de_N3_F1_B1.tsv"]
+        test_BT_de_4 = [basePath + "data/SNIPS/experiments/back_trans/de/test_fbwmt19_de_N3_F4_B4.tsv"]
+        test_BT_de_16 = [basePath + "data/SNIPS/experiments/back_trans/de/test_fbwmt19_de_N3_F16_B16.tsv"]
+        test_BT_de_64 = [basePath + "data/SNIPS/experiments/back_trans/de/test_fbwmt19_de_N3_F64_B64.tsv"]
+
         return {
             "clean": test_clean,
+            "test_BT_ru_1" : test_BT_ru_1,
+            "test_BT_ru_4" : test_BT_ru_4,
+            "test_BT_ru_16" : test_BT_ru_16,
+            "test_BT_ru_64" : test_BT_ru_64,
+            "test_BT_de_1" : test_BT_de_1,
+            "test_BT_de_4" : test_BT_de_4,
+            "test_BT_de_16" : test_BT_de_16, 
+            "test_BT_de_64" : test_BT_de_64
         }
     
     if test == "TOD":
 
         test_clean = [basePath + "data/TOD/experiments/clean/test/test.tsv"]
 
+        test_BT_ru_1 = [basePath + "data/TOD/experiments/back_trans/ru/test_fbwmt19_ru_N3_F1_B1.tsv"]
+        test_BT_ru_4 = [basePath + "data/TOD/experiments/back_trans/ru/test_fbwmt19_ru_N3_F4_B4.tsv"]
+        test_BT_ru_16 = [basePath + "data/TOD/experiments/back_trans/ru/test_fbwmt19_ru_N3_F16_B16.tsv"]
+        test_BT_ru_64 = [basePath + "data/TOD/experiments/back_trans/ru/test_fbwmt19_ru_N3_F64_B64.tsv"]
+
+        test_BT_de_1 = [basePath + "data/TOD/experiments/back_trans/de/test_fbwmt19_de_N3_F1_B1.tsv"]
+        test_BT_de_4 = [basePath + "data/TOD/experiments/back_trans/de/test_fbwmt19_de_N3_F4_B4.tsv"]
+        test_BT_de_16 = [basePath + "data/TOD/experiments/back_trans/de/test_fbwmt19_de_N3_F16_B16.tsv"]
+        test_BT_de_64 = [basePath + "data/TOD/experiments/back_trans/de/test_fbwmt19_de_N3_F64_B64.tsv"]
+
         return {
             "clean": test_clean,
+            "test_BT_ru_1" : test_BT_ru_1,
+            "test_BT_ru_4" : test_BT_ru_4,
+            "test_BT_ru_16" : test_BT_ru_16,
+            "test_BT_ru_64" : test_BT_ru_64,
+            "test_BT_de_1" : test_BT_de_1,
+            "test_BT_de_4" : test_BT_de_4,
+            "test_BT_de_16" : test_BT_de_16, 
+            "test_BT_de_64" : test_BT_de_64
         }
